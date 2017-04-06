d81fa54f8fc932ac28c3db699d3684183be45bc6
Satyaki Chakraborty <satyaki.cs15@gmail.com>
2017-04-06 13:48:09 +0530

    switched to tensorflow

--- model.py
+++ model.py
@@ -1,26 +1,66 @@
-from model_utils import add_conv_pool_layer
 import numpy as np
 import theano
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
+import tensorflow as tf
 
-def project_cnn(verbose=False):
-	model = Sequential()
-	model = add_conv_pool_layer(model, 16, (3, 64, 64))
-	model = add_conv_pool_layer(model, 32)
-	model = add_conv_pool_layer(model, 64)
-	model = add_conv_pool_layer(model, 128)
-	model = add_conv_pool_layer(model, 256)
-	model = add_conv_pool_layer(model, 512)
-	model.add(Flatten())
-	model.add(Dense(2, input_shape=(512,)))
-	if verbose==True:	print model.summary()
-	return model
+class cnn():
+	def __init__(self, input, labels, batch_size=50, is_training=False):
+		self.input_layer = tf.reshape(input, shape=[batch_size, 64, 64, 3])
+		self.conv1 = tf.layers.conv2d(
+      		inputs=self.input_layer,
+      		filters=16,
+      		kernel_size=[3, 3],
+      		padding="same",
+      		activation=tf.nn.relu)
+		self.pool1 = tf.layers.max_pooling2d(inputs=self.conv1, pool_size=[2, 2], strides=2)
+		
+		self.conv2 = tf.layers.conv2d(
+      		inputs=self.pool1,
+      		filters=32,
+      		kernel_size=[3, 3],
+      		padding="same",
+      		activation=tf.nn.relu)
+		self.pool2 = tf.layers.max_pooling2d(inputs=self.conv2, pool_size=[2, 2], strides=2)
+		
+		self.conv3 = tf.layers.conv2d(
+      		inputs=self.pool2,
+      		filters=64,
+      		kernel_size=[3, 3],
+      		padding="same",
+      		activation=tf.nn.relu)
+		self.pool3 = tf.layers.max_pooling2d(inputs=self.conv3, pool_size=[2, 2], strides=2)
+		
+		self.conv4 = tf.layers.conv2d(
+      		inputs=self.pool3,
+      		filters=128,
+      		kernel_size=[3, 3],
+      		padding="same",
+      		activation=tf.nn.relu)
+		self.pool4 = tf.layers.max_pooling2d(inputs=self.conv4, pool_size=[2, 2], strides=2)
+		
+		self.conv5 = tf.layers.conv2d(
+      		inputs=self.pool4,
+      		filters=256,
+      		kernel_size=[3, 3],
+      		padding="same",
+      		activation=tf.nn.relu)
+		self.pool5 = tf.layers.max_pooling2d(inputs=self.conv5, pool_size=[2, 2], strides=2)
+		
+		self.conv6 = tf.layers.conv2d(
+      		inputs=self.pool5,
+      		filters=512,
+      		kernel_size=[3, 3],
+      		padding="same",
+      		activation=tf.nn.relu)
+		self.pool6 = tf.layers.max_pooling2d(inputs=self.conv6, pool_size=[2, 2], strides=2)
+		
+		self.pool6_flat = tf.reshape(self.pool6, [batch_size, 512])
+  		self.dense1 = tf.layers.dense(inputs=self.pool6_flat, units=300, activation=tf.nn.tanh)
+  		self.dense2 = tf.layers.dense(inputs=self.dense1, units=180, activation=tf.nn.tanh)
+  		
+  		self.pred = tf.nn.softmax(self.dense2)
+  		self.correct_pred = tf.equal(tf.argmax(self.pred, 1), tf.argmax(labels, 1))
+		self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
+  		self.loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=self.dense2)
 
-def train_cnn():
-	model = project_cnn()
-	model.add(Dense(300, input_shape=(2,)))
-	model.add(Dense(180, input_shape=(300,), activation='softmax'))
-	print model.summary()
-	return model
+
--- train.py
+++ train.py
@@ -1,21 +1,141 @@
-from model import train_cnn
+from model import cnn
 import matplotlib.pyplot as plt 
 from PIL import Image
 import numpy as np
-from keras.callbacks import ModelCheckpoint
+import os
+import tensorflow as tf
+from etaprogress.progress import ProgressBar as pgb
 
-f = Image.open('rgb.jpg').convert('RGB')
-f = np.array(f, dtype='float32')/256.
-f = f.transpose(2, 0, 1)
-f = f.reshape(1, 3, 64, 64)
-f = np.concatenate([f, f])
-y = np.zeros((2, 180))
-y[0][0] = 1
-t_model = train_cnn()
-print t_model.predict(f).shape
 
-weights_file='cnn.hdf5'
-t_model.compile(loss='binary_crossentropy', optimizer='adam')
-checkpoint = ModelCheckpoint(weights_file, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
-t_model.fit(f, y, validation_split=0.5, nb_epoch=100, batch_size=1, callbacks=callbacks_list, verbose=1)
+# read files to create mappings
+# train dir name to class idx
+# val image name to class idx
+
+file = open('class_mapping.txt', 'rb')
+class_mapping_list = file.read().split()
+class_map = {}
+for i in range(0, 400, 2):	class_map[class_mapping_list[i]]=class_mapping_list[i+1]
+file.close()
+
+file = open('tiny-imagenet-200/val/val_annotations.txt', 'rb')
+annotations_list = file.read().split()
+val_annotations = {}
+for i in range(0, 60000, 6): val_annotations[annotations_list[i]]=int(class_map[annotations_list[i+1]])
+file.close()
+
+
+# one hot encoding
+# returns array of shape (1, dim)
+
+def one_hot(i, dim=180):
+	arr = np.zeros((1, dim))
+	arr[0][int(i)] = 1
+	return arr
+
+
+# read images from train directory
+# store data and labels
+
+train_dir = os.listdir('tiny-imagenet-200/train')
+train_data = ()
+train_labels = ()
+print '\n'
+for i in range(len(train_dir)):
+	print '\033[Floaded train data:', i*100./len(train_dir), '%'
+	im= os.listdir('tiny-imagenet-200/train/'+train_dir[i]+'/images')
+	for j in range(len(im)):
+		img = Image.open('tiny-imagenet-200/train/'+train_dir[i]+'/images/'+im[j]).convert('RGB')
+		img = np.array(img, dtype='float32')/256.
+		img = img.reshape(1, 64, 64, 3)
+		train_data +=(img, )
+		train_labels+=(one_hot(class_map[train_dir[i]]), )
+train_data = np.concatenate(train_data)
+train_labels = np.concatenate(train_labels)
+print train_data.shape, train_labels.shape
+
+
+# read images from validation directory
+# store data and labels
+
+valid_data = ()
+valid_labels = ()
+print '\n'
+valid_dir = os.listdir('tiny-imagenet-200/val/images')
+for j in range(len(valid_dir)):
+	print '\033[Floaded valid data:', j*100./len(valid_dir), '%'
+	img = Image.open('tiny-imagenet-200/val/images/'+valid_dir[j]).convert('RGB')
+	img = np.array(img, dtype='float32')/256.
+	img = img.reshape(1, 64, 64, 3)
+	class_idx = val_annotations[valid_dir[j]]
+	if class_idx >= 180:	continue
+	valid_data +=(img, )
+	valid_labels+=(one_hot(val_annotations[valid_dir[j]]), )
+valid_data = np.concatenate(valid_data)
+valid_labels = np.concatenate(valid_labels)
+print valid_data.shape, valid_labels.shape, '\n'
+
+
+# training begins here
+batch_size = 50
+
+input = tf.placeholder('float32', [batch_size, 64, 64, 3])
+label = tf.placeholder('uint8', [batch_size, 180])
+t_model = cnn(input, label)
+optimizer = tf.train.AdamOptimizer().minimize(t_model.loss)
+
+print 'starting training ...'
+n_epochs = 30
+n_samples = train_data.shape[0]
+n_val_samples = valid_data.shape[0]
+n_batches = n_samples/batch_size
+n_val_batches = n_val_samples/batch_size
+init_op = tf.global_variables_initializer()
+saver = tf.train.Saver()
+if not os.path.exists('./params'):	os.makedirs('./params')
+best_val_loss = np.inf
+if not os.path.exists('./logs/train'):	os.makedirs('./logs/train')
+if not os.path.exists('./logs/val'):	os.makedirs('./logs/val')
+tf.summary.scalar('loss', t_model.loss)
+tf.summary.scalar('accuracy', t_model.accuracy)
+merged = tf.summary.merge_all()
+
+
+with tf.Session() as sess:
+	sess.run(init_op)
+	if os.path.exists('./params/cnn_model.ckpt.meta'):
+		saver.restore(sess, './params/cnn_model.ckpt')
+		print 'model restored from previous checkpoint..'
+	train_writer = tf.summary.FileWriter('./logs/train', sess.graph)
+	val_writer = tf.summary.FileWriter('./logs/val', sess.graph)
+	print 'saving logs to ./logs/ ..'
+
+	for epoch in range(1, n_epochs+1):
+		print 
+		bar = pgb(n_batches, max_width=50)
+		for batch_idx in range(n_batches):
+			batch_x = train_data[batch_idx*batch_size:(batch_idx+1)*batch_size]
+			batch_y = train_labels[batch_idx*batch_size:(batch_idx+1)*batch_size]
+			train_batch_loss, train_acc, op, tr_summary = sess.run([t_model.loss, t_model.accuracy, optimizer, merged], feed_dict={input:batch_x, label:batch_y})
+			bar.numerator = batch_idx
+			print 'epoch:', epoch, bar, 'train loss:', train_batch_loss, 'accuracy:', train_acc, '\033[F'
+
+		val_losses=[]
+		val_accs = []
+		vbar = pgb(n_val_batches, max_width=50)
+		for vb_idx in range(n_val_batches):
+			v_batch_x = valid_data[vb_idx*batch_size:(vb_idx+1)*batch_size]
+			v_batch_y = valid_labels[vb_idx*batch_size:(vb_idx+1)*batch_size]
+			val_loss, v_acc, v_summary = sess.run([t_model.loss, t_model.accuracy, merged], feed_dict={input:v_batch_x, label:v_batch_y})
+			vbar.numerator = vb_idx
+			print 'epoch:', epoch, vbar, 'val loss:', val_loss, 'accuracy:', v_acc, '\033[F'
+			val_losses.append(val_loss)
+			val_accs.append(v_acc)
+
+		cur_val_loss = np.mean(val_losses)
+		cur_val_acc = np.mean(val_accs)
+		print '\nepoch:', epoch, 'mean validation loss:', cur_val_loss, '& mean validation acc:', cur_val_acc
+		if (cur_val_loss < best_val_loss):
+			print 'valdation loss improved!'
+			save_path = saver.save(sess, "./params/cnn_model.ckpt")
+			print("Best model saved in file: %s" % save_path)
+			best_val_loss = cur_val_loss
